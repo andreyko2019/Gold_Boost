9139865aaf468cee2250a63ae5ae03dbe221fd41
andreyko2019 <vip.andreyko@gmail.com>
2024-02-20 12:35:38 +0200

    init project

---
+++ storybook-static/assets/preview-DjjB3aXn.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-DzgEhSuG.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-B89dUOaW.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-B89dUOaW.js","./iframe-DzgEhSuG.js","./index-D0eRPQUM.js","./_commonjsHelpers-Cpj98o6Y.js","./index-Bw8VTzHM.js","./index-B8rCvqCN.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
