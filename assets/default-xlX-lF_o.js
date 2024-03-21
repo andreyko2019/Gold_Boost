41b8c16f038b0a2c411a1e4c2a1b035c453ac217
andreyko2019 <vip.andreyko@gmail.com>
2024-03-21 12:43:00 +0200

    Updates

--- assets/default-xlX-lF_o.js
+++
@@ -1,1 +0,0 @@
-const e="data:image/svg+xml,%3csvg%20width='124'%20height='103'%20viewBox='0%200%20124%20103'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3crect%20width='124'%20height='103'%20fill='%233C3E54'/%3e%3c/svg%3e";export{e as d};
